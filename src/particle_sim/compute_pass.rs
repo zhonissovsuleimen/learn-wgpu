e1a051d418f476b0d0345227a9394898eab6cc3c
Suleimen Zhonissov <zhonisov.s@gmail.com>
2026-03-20 16:33:38 +0500

    added window params to compute pass

--- src/particle_sim/compute_pass.rs
+++ src/particle_sim/compute_pass.rs
@@ -1,13 +1,14 @@
 use crate::{
   app::{gpu_wrapper::GpuWrapper, resources::Resources, window_wrapper::WindowWrapper},
   gpu_pass::{buffer_wrapper::BufferWrapper, gpu_pass::GpuPass},
-  particle_sim::{particle::Particle, shared::PARTICLES},
+  particle_sim::{particle::Particle, shared::PARTICLES, window::Window},
 };
 use std::time::Instant;
+use tracing::error;
 use wgpu::{
   BindGroup, BindGroupDescriptor, BindGroupEntry, BindGroupLayout, BindGroupLayoutDescriptor, BindGroupLayoutEntry, BindingType, Buffer,
   BufferBindingType, BufferSize, BufferUsages, CommandEncoder, ComputePassDescriptor, ComputePipeline, ComputePipelineDescriptor, Device,
-  PipelineLayoutDescriptor, ShaderStages, TextureView, include_wgsl,
+  PipelineLayoutDescriptor, Queue, ShaderStages, TextureView, include_wgsl,
   util::{BufferInitDescriptor, DeviceExt},
 };
 use winit::dpi::PhysicalSize;
@@ -15,6 +16,7 @@ use winit::dpi::PhysicalSize;
 #[derive(Default)]
 pub struct ComputePass {
   params_buffer: Option<Buffer>,
+  window_buffer: Option<Buffer>,
   pipeline: Option<ComputePipeline>,
 
   bind_group_layout: Option<BindGroupLayout>,
@@ -31,19 +33,30 @@ pub struct ComputePass {
 impl GpuPass for ComputePass {
   fn run(&mut self, encoder: &mut CommandEncoder, window: &WindowWrapper, gpu: &GpuWrapper, _view: &TextureView, resources: &mut Resources) {
     let (_, _, device, queue) = gpu.into();
-
     let params_buffer = self.params_buffer.get_or_insert_with(|| ComputePass::init_params_buffer(device));
+
+    if self.window_buffer.is_none() {
+      let Some(buf) = ComputePass::init_window_buffer(device, window) else {
+        error!("Platform not supported: could not get window position");
+        return;
+      };
+      self.window_buffer = Some(buf);
+    }
+    let window_buffer = self.window_buffer.as_ref().unwrap();
+    ComputePass::update_window_buffer(&gpu.queue, window_buffer, window);
+
     let particle_buffer_a = self.buffer_a.get_or_insert_with(|| ComputePass::init_buffer(device, window));
     let particle_buffer_b = self.buffer_b.get_or_insert_with(|| ComputePass::init_buffer(device, window));
 
-    let bind_group_layout = self
-      .bind_group_layout
-      .get_or_insert_with(|| ComputePass::init_bind_group_layout(device, params_buffer, &particle_buffer_a.buffer, &particle_buffer_b.buffer));
+    let bind_group_layout = self.bind_group_layout.get_or_insert_with(|| {
+      ComputePass::init_bind_group_layout(device, params_buffer, window_buffer, &particle_buffer_a.buffer, &particle_buffer_b.buffer)
+    });
     let bind_group_a = self.bind_group_a.get_or_insert_with(|| {
       ComputePass::init_bind_group_a(
         device,
         bind_group_layout,
         params_buffer,
+        window_buffer,
         &particle_buffer_a.buffer,
         &particle_buffer_b.buffer,
       )
@@ -53,6 +66,7 @@ impl GpuPass for ComputePass {
         device,
         bind_group_layout,
         params_buffer,
+        window_buffer,
         &particle_buffer_a.buffer,
         &particle_buffer_b.buffer,
       )
@@ -107,6 +121,27 @@ impl ComputePass {
     })
   }
 
+  fn init_window_buffer(device: &Device, window: &WindowWrapper) -> Option<Buffer> {
+    let Ok(top_left) = window.window.inner_position() else {
+      return None;
+    };
+    let size = window.window.inner_size().cast::<f32>();
+
+    let top_left = [top_left.x as f32, top_left.y as f32];
+    let bottom_right = [top_left[0] + size.width, top_left[1] + size.height];
+
+    let win = Window { top_left, bottom_right };
+
+    let data = [win.top_left, win.bottom_right];
+    let buffer = device.create_buffer_init(&BufferInitDescriptor {
+      label: Some("Window Buffer"),
+      contents: bytemuck::bytes_of(&data),
+      usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
+    });
+
+    Some(buffer)
+  }
+
   fn init_buffer(device: &Device, window: &WindowWrapper) -> BufferWrapper<Particle> {
     let size = match window.window.current_monitor() {
       Some(monitor) => monitor.size().cast::<f32>(),
@@ -133,7 +168,13 @@ impl ComputePass {
     BufferWrapper::new(buffer, count as u32)
   }
 
-  fn init_bind_group_layout(device: &Device, params_buffer: &Buffer, particle_buffer_a: &Buffer, particle_buffer_b: &Buffer) -> BindGroupLayout {
+  fn init_bind_group_layout(
+    device: &Device,
+    params_buffer: &Buffer,
+    window_buffer: &Buffer,
+    particle_buffer_a: &Buffer,
+    particle_buffer_b: &Buffer,
+  ) -> BindGroupLayout {
     device.create_bind_group_layout(&BindGroupLayoutDescriptor {
       label: Some("Compute bind group layout"),
       entries: &[
@@ -151,6 +192,16 @@ impl ComputePass {
           binding: 1,
           visibility: ShaderStages::COMPUTE,
           ty: BindingType::Buffer {
+            ty: BufferBindingType::Uniform,
+            has_dynamic_offset: false,
+            min_binding_size: BufferSize::new(window_buffer.size()),
+          },
+          count: None,
+        },
+        BindGroupLayoutEntry {
+          binding: 2,
+          visibility: ShaderStages::COMPUTE,
+          ty: BindingType::Buffer {
             ty: BufferBindingType::Storage { read_only: true },
             has_dynamic_offset: false,
             min_binding_size: BufferSize::new(particle_buffer_a.size()),
@@ -158,7 +209,7 @@ impl ComputePass {
           count: None,
         },
         BindGroupLayoutEntry {
-          binding: 2,
+          binding: 3,
           visibility: ShaderStages::COMPUTE,
           ty: BindingType::Buffer {
             ty: BufferBindingType::Storage { read_only: false },
@@ -175,6 +226,7 @@ impl ComputePass {
     device: &Device,
     bind_group_layout: &BindGroupLayout,
     params_buffer: &Buffer,
+    window_buffer: &Buffer,
     particle_buffer_a: &Buffer,
     particle_buffer_b: &Buffer,
   ) -> BindGroup {
@@ -188,10 +240,14 @@ impl ComputePass {
         },
         BindGroupEntry {
           binding: 1,
-          resource: particle_buffer_a.as_entire_binding(),
+          resource: window_buffer.as_entire_binding(),
         },
         BindGroupEntry {
           binding: 2,
+          resource: particle_buffer_a.as_entire_binding(),
+        },
+        BindGroupEntry {
+          binding: 3,
           resource: particle_buffer_b.as_entire_binding(),
         },
       ],
@@ -202,6 +258,7 @@ impl ComputePass {
     device: &Device,
     bind_group_layout: &BindGroupLayout,
     params_buffer: &Buffer,
+    window_buffer: &Buffer,
     particle_buffer_a: &Buffer,
     particle_buffer_b: &Buffer,
   ) -> BindGroup {
@@ -215,10 +272,14 @@ impl ComputePass {
         },
         BindGroupEntry {
           binding: 1,
-          resource: particle_buffer_b.as_entire_binding(),
+          resource: window_buffer.as_entire_binding(),
         },
         BindGroupEntry {
           binding: 2,
+          resource: particle_buffer_b.as_entire_binding(),
+        },
+        BindGroupEntry {
+          binding: 3,
           resource: particle_buffer_a.as_entire_binding(),
         },
       ],
@@ -242,5 +303,17 @@ impl ComputePass {
       compilation_options: Default::default(),
       cache: None,
     })
+  }
+
+  fn update_window_buffer(queue: &Queue, window_buffer: &Buffer, window: &WindowWrapper) {
+    let Ok(top_left) = window.window.inner_position() else {
+      return;
+    };
+    let size = window.window.inner_size().cast::<f32>();
+    let data: [[f32; 2]; 2] = [
+      [top_left.x as f32, top_left.y as f32],
+      [top_left.x as f32 + size.width, top_left.y as f32 + size.height],
+    ];
+    queue.write_buffer(window_buffer, 0, bytemuck::bytes_of(&data));
   }
 }
--- src/particle_sim/shaders/move.wgsl
+++ src/particle_sim/shaders/move.wgsl
@@ -7,9 +7,15 @@ struct Particle {
   vel: vec2<f32>,
 };
 
+struct Window {
+  top_left: vec2<f32>,
+  bottom_right: vec2<f32>,
+}
+
 @group(0) @binding(0) var<uniform> params: Params;
-@group(0) @binding(1) var<storage, read> particlesSrc: array<Particle>;
-@group(0) @binding(2) var<storage, read_write> particlesDst: array<Particle>;
+@group(0) @binding(1) var<uniform> window: Window;
+@group(0) @binding(2) var<storage, read> particlesSrc: array<Particle>;
+@group(0) @binding(3) var<storage, read_write> particlesDst: array<Particle>;
 
 @compute
 @workgroup_size(64)
